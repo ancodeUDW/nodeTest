ee0647588dc23a12fbee2c24bc921c4375c0a021
ramon2Mayordomo <ramon2@mayordomo.io>
2020-03-16 20:06:27 +0100

    test of npm 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World ' + R.T ? 'ramda is online' : 'error of ramda');
+    res.end('Hello World ' + (R.T ? 'ramda is online' : 'error of ramda'));
 });
 
 server.listen(port, hostname, () => {
